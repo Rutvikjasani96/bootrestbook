b7dcff41fd0df1c2b08c164f17159d3d95aeccc1
Rutvik Jasani <rutvikjasani96@gmail.com>
2024-07-29 18:04:41 +0530

    PostMapping, DeleteMapping & PutMapping in SpringBoot is Completed, CRUD done...

--- src/main/java/com/bootrestbook/controllers/BookController.java
+++ src/main/java/com/bootrestbook/controllers/BookController.java
@@ -3,8 +3,12 @@ package com.bootrestbook.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bootrestbook.etities.Book;
@@ -24,4 +28,21 @@ public class BookController {
     public Book getBookById(@PathVariable("id") Integer id){
         return bookService.getBookbyId(id);
     }
+
+
+    @PostMapping("/books")
+    public Book addBook(@RequestBody Book book){
+        return bookService.addBook(book);
+    }
+
+    @DeleteMapping("/books/{deleteId}")
+    public void deleteBook(@PathVariable("deleteId") Integer deleteId){
+        bookService.deleteBook(deleteId);
+    }
+
+    @PutMapping("/books/{updateId}")
+    public Book updateBook(@RequestBody Book book, @PathVariable("updateId") Integer updateId){
+        return bookService.updateBook(book, updateId);
+    }
+
 }
--- src/main/java/com/bootrestbook/services/BookService.java
+++ src/main/java/com/bootrestbook/services/BookService.java
@@ -2,6 +2,8 @@ package com.bootrestbook.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
@@ -12,23 +14,45 @@ public class BookService {
     static List<Book> booksList = new ArrayList<>();
     private Book book;
 
-    static{
-        booksList.add(new Book(1,"Java Learning","RJ"));
-        booksList.add(new Book(2,"Python Learning","Sen"));
-        booksList.add(new Book(3,"Spring Boot Learning","Rutvik Jasani"));
-        booksList.add(new Book(4,"Flutter Learning","IIT"));
-        booksList.add(new Book(5,"Dart Learning","NIT"));
+    static {
+        booksList.add(new Book(1, "Java Learning", "RJ"));
+        booksList.add(new Book(2, "Python Learning", "Sen"));
+        booksList.add(new Book(3, "Spring Boot Learning", "Rutvik Jasani"));
+        booksList.add(new Book(4, "Flutter Learning", "IIT"));
+        booksList.add(new Book(5, "Dart Learning", "NIT"));
     }
 
     // Get All the Books
-    public List<Book> getAllBooks(){
+    public List<Book> getAllBooks() {
         return booksList;
     }
 
     // Find any one Book by Id
-    public Book getBookbyId(int id){
-        Book book = booksList.stream().filter(bk->bk.getId()==id).findFirst().get();
+    public Book getBookbyId(int id) {
+        Book book = booksList.stream().filter(bk -> bk.getId() == id).findFirst().get();
         return book;
     }
-    
+
+    // Add New Book
+    public Book addBook(Book book) {
+        booksList.add(book);
+        return book;
+    }
+
+    // Delete Book
+    public void deleteBook(int deleteId) {
+        booksList = booksList.stream().filter(book -> book.getId() != deleteId).collect(Collectors.toList());
+    }
+
+    // update Book
+    public Book updateBook(Book book,int updateId){
+        booksList = booksList.stream().map(bk->{
+            if(bk.getId()==updateId){
+                bk=book;
+            }
+            return bk;
+        }).collect(Collectors.toList());
+        return book;
+    }
+
 }
